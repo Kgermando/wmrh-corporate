2a68a184324cfc9e1921cace48794197a0a2f944
Kgermando <katakugermain@gmail.com>
2024-01-05 12:54:10 +0100

    fixing url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV;
 
 export const environment = {  
   apiURL: (env  === 'production') 
-    ? 'https://wmrh-corporate-api-3c92ba752eca.herokuapp.com/api'
+    ? 'https://wmrh-corporate-api-production.up.railway.app/api'
     : 'http://localhost:3001/api',
 
   publicKey: "BHnrukMOoUpozT0O0LK9g_snE-nSCM_XeoEfbsy3FJO5vJQIAk5TeSYqol0HlvMUU-3poVLx1xNl8nAv14JVoL4",
